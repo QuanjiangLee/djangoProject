10e4c75d17515ceae57053797aaadd68d499dfdb
Quanjiang Lee <quanjianglee@gmail.com>
2017-04-21 16:51:14 +0800

    优化脚本和一些所需文件

--- run.sh
+++ run.sh
@@ -1,30 +1,53 @@
-#!/bin/bash 
+#! /bin/bash 
+#PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+#export PATH
 
-clear;
-yum install -y vim;
+#clear
+
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+     echo "错误:请在root用户权限下运行!" 1>&2;
+     exit 1;
+fi
+
+read -r -p "您确定要运行安装项目程序吗? [Y/n] " input
+case $input in
+    [yY][eE][sS]|[yY])
+            echo "您选择了YES!"
+            ;;
+*)
+        echo "安装停止！"
+        exit 1;
+        ;;
+esac
+
+clear
+
+yum install -y vim
 
 #安装python3和pip3
-xz -d Python-3.4.6.tar.xz;
-tar -xvf Python-3.4.6.tar.xz;
-cd Python-3.4.6;
-yum groupinstall -y 'Development Tools';
-yum install -y zlib-devel bzip2-devel openssl-devel ncurese-devel;
-./configure --prefix=/usr/local/python3;
-make && make install;
+#xz -d Python-3.4.6.tar.xz
+tar -xvf Python-3.4.6.tar.xz
+cd Python-3.4.6
+yum groupinstall -y 'Development Tools'
+yum install -y zlib-devel bzip2-devel openssl-devel ncurese-devel
+./configure --prefix=/usr/local/python3
+make && make install
 cd ..
 
 #软链接python3和pip3到环境变量
-ln -s /usr/local/python3/bin/python3 /usr/bin/python3;
-ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3;
+ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
 
 #安装django 1.10.5 版
-pip3 install Django==1.10.5;
-pip3 install pymysql;
-pip3 install xlrd;
+pip3 install Django==1.10.5
+pip3 install pymysql
+pip3 install xlrd
 #安装mysql-community
-rpm -ivh mysql-community-release-el7-5.noarch.rpm;
-yum repolist enabled | grep "mysql.*-community*";
-yum install -y mysql -community-server;
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum repolist enabled | grep "mysql.*-community*"
+yum install -y mysql -community-server
 
 #设置mysql
 mysql -u root -h localhost password 'xaut.qll'
@@ -38,25 +61,25 @@ flush privileges;
 quit;
 EOF
 #导入数据库文件
+mysql -uroot -p'xaut.qll' safeDb < safeDbstruc.sql
+mysql -uroot -p'xaut.qll' session < sessionstruc.sql
 
-
-
-systemctl restart mysqld;
+systemctl restart mysqld
 #安装nginx
-rpm -ivh nginx-1.11.0-1.el7.ngx.x86_64.rpm;
+rpm -ivh nginx-1.11.0-1.el7.ngx.x86_64.rpm
 #sudo yum install epel-release
 #sudo yum install python-devel nginx
-mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bakconf;
+mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bakconf
 cp ./httpssl.conf /etc/nginx/conf.d/httpssl.conf
-systemctl restart nginx;
+systemctl restart nginx
 
 #安装配置supervisord 和wsgi
-pip3 install supervisor;
-pip install uwsgi --upgrade;
+pip3 install supervisor
+pip install uwsgi --upgrade
 cp ./supervisord.conf /etc/supervisord.conf
 supervisord -c /etc/supervisord.conf
-supervisorctl -c /etc/supervisord.conf start djangoWeb;
-clear;
+supervisorctl -c /etc/supervisord.conf start djangoWeb
+clear
 
 echo"安装完成啦。。。"
 
