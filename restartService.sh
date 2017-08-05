d01109bb86a2988fe6658ab74e9db506d3343579
Quanjiang Lee <QuanjiangLee@gmail.com>
2017-08-06 02:27:28 +0800

    添加修改文件

--- restartService.sh
+++ restartService.sh
@@ -2,6 +2,14 @@
 # add for chkconfig
 #description:auto restart
 sleep 15
-echo "hello" > `pwd`\/test.txt
+#UUID='EAA7-A356'
+#Device=`blkid | grep $UUID | cut -d : -f 1`
+#if [ $Device ]; then
+#mount $Device /mnt
+#cp /mnt/check_ukey .
+#./check_ukey t 
+#rm check_ukey
+#else echo "No Ukey devices found!"
+#fi
 supervisorctl -c /etc/supervisord.conf restart all
 
